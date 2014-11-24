b0228212c6b1249b4aab40e60bf0972294fe5419
Samuli Thomasson <simsaladin@paivola.fi>
2014-11-24 16:08:52 +0200

    simple table render

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 ------------------------------------------------------------------------------
 -- | 
 -- Module         : Main
@@ -14,6 +15,7 @@ import Prelude hiding (readFile)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Monoid ((<>))
+import Data.Function (on)
 import Control.Monad
 import Text.Printf
 import Data.Maybe
@@ -21,10 +23,13 @@ import Network.HTTP.Conduit (simpleHttp)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy.IO as LT
 import qualified Data.List as L
 import Text.XML
 import Text.XML.Cursor
 import Debug.Trace
+import Text.Hamlet
+import Text.Blaze.Renderer.Text (renderMarkup)
 
 -- * Configure
 
@@ -39,6 +44,12 @@ categories =
     , [ "A. ", "B. " ]
     , [ "kevät", "syksy" ]
     ]
+
+mainCat :: Course -> Text
+mainCat (cats, _)
+    | Just c <- L.find (\x -> T.head x `elem` ['0'..'9']) cats = c
+    | otherwise = "Fysiikka"
+    -- ^ TODO this thing makes no sense whatsoever
 
 -- * main
 
@@ -89,10 +100,10 @@ accumCategory c cs = L.nub $ c : maybe (c : cs) (L.deleteFirstsBy T.isInfixOf cs
 -- | A row is either a category or course
 getRow :: [Header] -> [Category] -> [Cursor] -> ([Category], Maybe Course)
 getRow hs cats = go . map ($// content) -- ($// content)
-    where go :: [ [Text] ] -> ([Category], Maybe Course)
+    where go :: [[Text]] -> ([Category], Maybe Course)
           go (mc : vs) = case toCategory (head mc) of
                 Just cat -> (accumCategory cat cats, Nothing)
-                Nothing  -> (cats, Just $ toCourse cats hs $ map head vs)
+                Nothing  -> (cats, Just $ toCourse cats hs $ map T.unwords vs)
 
 toCategory :: Text -> Maybe Category
 toCategory t = do guard $ t /= "\160" && t /= "syksy" && t /= "kevät"
@@ -111,23 +122,47 @@ parseSettings = def { psDecodeEntities = decodeHtmlEntities }
 processDoc :: Document -> IO ()
 processDoc = renderTable . head . catMaybes . findTable . fromDocument
 
-renderTable :: Table -> IO ()
-renderTable (Table hs cs) = do
-    -- mapM_ T.putStrLn hs
-    mapM_ ppCourse cs
-
 findTable :: Cursor -> [Maybe Table]
 findTable = element "table" &| processTable
 
 getHeader :: Cursor -> Maybe Header
-getHeader = go . normalize . head . ($// content)
+getHeader = go . normalize . T.unwords . ($// content)
     where go "" = Nothing
           go x = Just x
 
 -- * Output
+
+renderTable :: Table -> IO ()
+renderTable t@(Table hs cs) = do
+    LT.putStrLn $ renderMarkup $ listed t
+    -- mapM_ ppCourse cs
 
 ppCourse :: Course -> IO ()
 ppCourse (cats, vals) = do
     T.putStrLn " ---------------------------------------- "
     printf "%-28s: %s\n" ("Categories" :: String) (T.unpack $ T.intercalate ", " cats)
     mapM_ (\(h, v) -> printf "%-28s: %s\n" (T.unpack h) (T.unpack v)) $ Map.toList vals
+
+listed :: Table -> Html
+listed (Table _ stuff) = [shamlet|
+$forall courses <- L.groupBy mainCategory stuff
+    <h1>#{mainCat $ head courses}
+    <table>
+        $forall c <- courses
+            <tr>
+                <td style="width:67%">#{getThing "Koodi" c} #{getThing "Kurssin nimi" c}
+                <td style="width:15%">
+                    $maybe p <- getThingMaybe "Kotisivu" c
+                        <a href="#{p}">Kotisivu
+                <td style="width:13%">
+                    <a href="https://weboodi.helsinki.fi/hy/opintjakstied.jsp?html=1&Kieli=1&Tunniste=#{getThing "Koodi" c}">Kuvaus
+|]
+
+getThing :: Text -> Course -> Text
+getThing k = fromMaybe (error $ "Key not found: " ++ T.unpack k) . getThingMaybe k
+
+getThingMaybe :: Text -> Course -> Maybe Text
+getThingMaybe k (_, c) = Map.lookup k c
+
+mainCategory :: Course -> Course -> Bool
+mainCategory = (==) `on` mainCat
