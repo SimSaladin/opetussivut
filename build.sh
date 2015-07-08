35a1fd35e714f96e5ba3f63ce9298e151ee4f16c
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2015-07-09 01:48:32 +0300

    Added scripts to help with building the project.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------
+# Build the project with 'cabal install'. Run the executable to see
+# if it is working.
+#-------------------------------------------------------------------
+
+cabal install
+
